23fd7c4a743b9b031cd32c1eb6a55db495a2337f
PoojaSable6 <poojasable9523@gmail.comgit config --global user.email poojasable9523@gmail.com>
2023-10-26 21:56:44 +0530

    This is datetime eighth rep commit message

---
+++ eighth_rep.py
@@ -0,0 +1,9 @@
+import datetime
+z = datetime.datetime(1947, 8, 15, 3, 1, 23)#user defined date
+print(z.year)
+print(z.month)
+print(z.day)
+print(z.hour)
+print(z.minute)
+print(z.second)
+print(z.strftime("%B"))
